4aca9c6c4127f3d47bc18df3453ec7199771774a
abhi0516 <90794084+abhi0516@users.noreply.github.com>
2022-09-21 12:52:57 -0500

    food java created commit

---
+++ app/src/main/java/com/example/mad_cafe/Food.java
@@ -0,0 +1,57 @@
+package com.example.mad_cafe;
+
+public class Food {
+    private String name;
+    private double price;
+    private String desc;
+    private int imageResourceID;
+
+
+    //used code -> generate -> constructor
+
+    public Food(String name, double price, String desc, int imageResourceID) {
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+        this.imageResourceID = imageResourceID;
+    }
+
+    // the toString method tells java what should be returned when we try to print a food object
+    public String toString(){
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+
+    public void setImageResourceID(int imageResourceID) {
+        this.imageResourceID = imageResourceID;
+    }
+
+
+}
